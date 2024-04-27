cba5d705f0a608daabfef95aa5279524bff09ddb
marcosricciardi12 <marcosricciardi12@gmail.com>
2024-04-26 22:39:50 -0300

    clean repo

---
+++ extra/mouse_coords.sh
@@ -0,0 +1,4 @@
+while true; do     clear  # Limpiar la pantalla para actualizar las coordenadas
+    xdotool getmouselocation  # Obtener las coordenadas del mouse
+    sleep 0.1  # Esperar un breve período de tiempo antes de actualizar nuevamente
+done
